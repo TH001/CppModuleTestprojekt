0370c79e19589b4176e285015006ba28c1e41c2e
TH0001 <tomheine99@googlemail.com>
2019-05-06 11:44:42 +0200

    Add project files.

---
+++ CppModuleTestprojekt/CppModuleTestprojekt.cpp
@@ -0,0 +1,12 @@
+﻿// CppModuleTestprojekt.cpp : Defines the entry point for the application.
+//
+
+#include "CppModuleTestprojekt.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
